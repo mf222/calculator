5566910031463288085538afb4c64d0ad02d0ca4
JaimePata <jsyanez@uc.cl>
2016-10-20 17:55:16 -0300

    rip

--- calculator_test.rb
+++ calculator_test.rb
@@ -9,22 +9,22 @@ class TestCalculator < Minitest::Test
 
   def test_calculate_answer_add
     respuesta = @calculator.calculate_answer('add', 1, 1)
-    assert_equal respuesta, 2
+    assert_equal 2,respuesta
   end
 
   def test_calculate_answer_substract
-    respuesta = @calculator.calculate_answer('substract', 5, 1)
-    assert_equal respuesta, 4
+    respuesta = @calculator.calculate_answer('subtract', 7, 1)
+    assert_equal 6,respuesta
   end
 
   def test_calculate_answer_mutiply
     respuesta = @calculator.calculate_answer('multiply', 5, 1)
-    assert_equal respuesta, 5
+    assert_equal 5,respuesta
   end
 
   def test_calculate_answer_divide
     respuesta = @calculator.calculate_answer('divide', 5, 1)
-    assert_equal respuesta, 5
+    assert_equal 5,respuesta
   end
 
   def test_inexistant_operation
@@ -37,32 +37,32 @@ class TestCalculator < Minitest::Test
 
   def test_request_calculation_type_1
     respuesta = @calculator.request_calculation_type(1)
-    assert_equal respuesta, 'add'
+    assert_equal 'add',respuesta
   end
 
   def test_request_calculation_type_2
     respuesta = @calculator.request_calculation_type(2)
-    assert_equal respuesta, 'subtract'
+    assert_equal 'subtract',respuesta
   end
 
   def test_request_calculation_type_3
     respuesta = @calculator.request_calculation_type(3)
-    assert_equal respuesta, 'multiply'
+    assert_equal 'multiply', respuesta
   end
 
   def test_request_calculation_type_4
     respuesta = @calculator.request_calculation_type(4)
-    assert_equal respuesta, 'divide'
+    assert_equal 'divide', respuesta
   end
 
   def test_request_calculation_type_other_str
     respuesta = @calculator.request_calculation_type('hansfindel')
-    assert_equal respuesta, 'error'
+    assert_equal 'error', respuesta
   end
 
   def test_request_calculation_type_other_number
     respuesta = @calculator.request_calculation_type(-1)
-    assert_equal respuesta, 'error'
+    assert_equal 'error', respuesta
   end
 
 end
